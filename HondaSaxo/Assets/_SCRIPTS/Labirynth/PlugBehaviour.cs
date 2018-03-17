5e811fc910da5954d5fdfe5f5e08a28f0b209e4d
Rafikus <Rafikus95@gmail.com>
2018-03-17 06:11:49 +0100

    Dodano możliwość umierania w labiryncie

--- HondaSaxo/Assets/_SCRIPTS/Labirynth/PlugBehaviour.cs
+++ HondaSaxo/Assets/_SCRIPTS/Labirynth/PlugBehaviour.cs
@@ -1,45 +1,75 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlugBehaviour : MonoBehaviour
 {
     public float speed = 5f;
     public float rotationSpeed = 5f;
+    private float timeout = 5f;
     private IEnumerator coroutine;
 
+    private ArrayList colliders;
+
+    void Start()
+    {
+        colliders = new ArrayList();
+    }
+
     // Update is called once per frame
-    void Update () {
+    void Update ()
+    {   
+        if(timeout > 0) timeout -= Time.deltaTime;
 		transform.Translate(Vector3.up * Time.deltaTime * speed);
 	    transform.Rotate(Vector3.back, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
         if (Time.time % .1f <= 0.01f)
         {
             GameObject gameObject = new GameObject("collider");
+
+            colliders.Add(gameObject);
+
             Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
+
             rb.gravityScale = 0;
+            
             gameObject.transform.position = transform.position;
+
             CircleCollider2D col = gameObject.AddComponent<CircleCollider2D>();
             col.enabled = false;
-            coroutine = enableCollider(col);
-            StartCoroutine(coroutine);
+
             col.radius = 0.06f;
             gameObject.transform.tag = "Player";
+
+            coroutine = enableCollider(col);
+            StartCoroutine(coroutine);
+
         }
     }
 
     IEnumerator enableCollider(CircleCollider2D col)
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(.5f);
         col.enabled = true;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Player")
+        
+        if (collision.transform.tag == "Player" && timeout <= 0)
         {
+            timeout = 5f;
             LabirynthManager labirynthManager =
                 GameObject.Find("LabirynthManager").gameObject.GetComponent<LabirynthManager>();
 
             labirynthManager.loseLife();
+        }
+    }
+
+    void OnDestroy()
+    {
+        foreach (GameObject col in colliders)
+        {
+            Destroy(col);
         }
     }
 }
