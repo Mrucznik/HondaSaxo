95c1e4fd4e6ab0ef6e61a222a07388bbc45779e2
Rafikus <Rafikus95@gmail.com>
2018-03-17 04:41:25 +0100

    Superman

--- HondaSaxo/Assets/_SCRIPTS/Labirynth/PlugBehaviour.cs
+++ HondaSaxo/Assets/_SCRIPTS/Labirynth/PlugBehaviour.cs
@@ -6,29 +6,32 @@ public class PlugBehaviour : MonoBehaviour
     public float speed = 5f;
     public float rotationSpeed = 5f;
 
-    IEnumerator WaitAndPrint()
-    {
-        // suspend execution for 5 seconds
-        yield return new WaitForSeconds(.1f);
-        print("WaitAndPrint " + Time.time);
-    }
-
-    IEnumerator Start()
-    {
-        print("Starting " + Time.time);
-
-        // Start function WaitAndPrint as a coroutine
-        yield return StartCoroutine("WaitAndPrint");
-        print("Done " + Time.time);
-    }
 
     // Update is called once per frame
-    void Update () {
+    IEnumerable Update () {
 		transform.Translate(Vector3.up * Time.deltaTime * speed);
 	    transform.Rotate(Vector3.back, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        if (Time.time % .1f <= 0.01f)
+        {
+            GameObject gameObject = new GameObject("collider");
+            Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            gameObject.transform.position = transform.position;
+            CircleCollider2D col = gameObject.AddComponent<CircleCollider2D>();
+            col.enabled = false;
+            coroutine = enableCollider(col);
+            StartCoroutine(coroutine);
+            col.radius = 0.06f;
+            gameObject.transform.tag = "Player";
+        }
     }
 
-    void OnCollisionDetect(Collision collision)
+    IEnumerable enableCollider(CircleCollider2D col)
+    {
+        col.enabled = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Player")
         {
