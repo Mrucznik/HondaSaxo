6ab75cec31b6927ba68fdcbed92151e659a80b64
Mrucznik <mrucznix@gmail.com>
2018-03-17 01:48:28 +0100

    Some shit

---
+++ HondaSaxo/Assets/_SCRIPTS/Story/DialogChoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets._SCRIPTS.Story
+{
+    public class DialogChoice : IDialog
+    {
+        private List<DialogLine> _choices;
+        private List<IDialog> lol;
+
+        public DialogChoice()
+        {
+            this._choices = new List<DialogLine>();
+        }
+
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HondaSaxo/Assets/_SCRIPTS/Story/DialogLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets._SCRIPTS.Story
+{
+    public class DialogLine : IDialog
+    {
+        public Character Interlocutor { get; private set; }
+        public string Text { get; private set; }
+
+        public DialogLine(Character interlocutor, string text)
+        {
+            Interlocutor = interlocutor;
+            Text = text;
+        }
+
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
