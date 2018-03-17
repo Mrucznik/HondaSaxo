eab17ca76bffaf12b7c51f062b2d903d0cbffd8f
Rafikus <Rafikus95@gmail.com>
2018-03-17 03:30:07 +0100

    Joker

---
+++ HondaSaxo/Assets/_SCRIPTS/Labirynth/PlugBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlugBehaviour : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 5f;
+
+    IEnumerator WaitAndPrint()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(.1f);
+        print("WaitAndPrint " + Time.time);
+    }
+
+    IEnumerator Start()
+    {
+        print("Starting " + Time.time);
+
+        // Start function WaitAndPrint as a coroutine
+        yield return StartCoroutine("WaitAndPrint");
+        print("Done " + Time.time);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		transform.Translate(Vector3.up * Time.deltaTime * speed);
+	    transform.Rotate(Vector3.back, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionDetect(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            LabirynthManager labirynthManager =
+                GameObject.Find("LabirynthManager").gameObject.GetComponent<LabirynthManager>();
+
+            labirynthManager.loseLife();
+        }
+    }
+}
--- HondaSaxo/Assets/_SCRIPTS/Story/Choice.cs
+++ HondaSaxo/Assets/_SCRIPTS/Story/Choice.cs
@@ -11,7 +11,7 @@ namespace Assets._SCRIPTS.Story
         private string _text;
         private Button _button;
 
-        public Choice(string text)
+        public Choice(string text, Button button)
         {
             this._text = text;
             this._button = button;
