40679e518630be2163db57f151d78753b619fe0d
U-DESKTOP-IB7GL2T\Parada <paradowskim94@gmail.com>
2018-03-17 04:15:27 +0100

    jakies dziwne proby

---
+++ HondaSaxo/Assets/_SCRIPTS/CityMap/CityMapBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityMapBehaviour : MonoBehaviour {
+    public float speed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+       /* transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.right * Time.deltaTime, Space.World);*/
+    }
+}
