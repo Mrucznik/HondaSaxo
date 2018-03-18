d89fec116cf67cc4f59acf8251240d15edaa9a67
Rafikus <Rafikus95@gmail.com>
2018-03-18 05:19:35 +0100

    Poczatek dołączenia labiryntów

---
+++ HondaSaxo/Assets/_SCRIPTS/Story/MineLabirynthDialog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets._SCRIPTS.Story
+{
+    class MineLabirynthDialog : IDialog
+    {
+        public void Display()
+        {
+            SceneManager.LoadScene(7);
+        }
+
+        public IDialog GetNextDialog()
+        {
+            return null;
+        }
+    }
+}
